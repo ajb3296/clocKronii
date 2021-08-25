07998148cc7a7320ae704247cdffda056b823379
천상의나무 <ajb8533296@gmail.com>
2021-08-25 16:32:14 +0900

    clocKronii

---
+++ README.md
@@ -0,0 +1,10 @@
+# clocKronii
+
+This is a clock made for Ouro Kronii([Youtube](https://www.youtube.com/channel/UCmbs8T6MWqUHP1tIQvSgKrg), [Twitter](https://twitter.com/ourokronii))<br><br>
+
+I used the [Julius Sans One](https://fonts.google.com/specimen/Julius+Sans+One?query=Julius) font.
+
+### ScreenShot
+
+![clocKronii1](screenshot1.png)
+![clocKronii2](screenshot2.png)
---
+++ js/script.js
@@ -0,0 +1,36 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+let date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+function runTheClock() {
+
+    hrPosition = hrPosition+(3/360);
+    minPosition = minPosition+(6/60);
+    secPosition = secPosition+6;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
+
+
+document.addEventListener('scroll', function() {
+    var currentScrollValue = document.documentElement.scrollTop;
+    if (currentScrollValue/10 < 50 && currentScrollValue > -5) {
+        document.getElementById('clock').style.transform = "perspective(500px) rotateX(" + currentScrollValue/10 + "deg)"
+        document.getElementById('clock').style.filter = "drop-shadow( " +currentScrollValue/80+ "px " + currentScrollValue/10 + "px 5px #CCCCCC)"
+    }
+});
